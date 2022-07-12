dc045038dcfe41432f5c977404901f8ae1e9e594
Jonathan Costa <jonathancosta746@gmail.com>
2022-07-12 15:37:02 -0300

    adicionado swipper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,27 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () =>{
     const header = document.getElementById('header')
-    
+
     this.scrollY >= 50 ? header.classList.add('scroll-header') 
                        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*=============== SWIPER POPULAR ===============*/
 
+/*=============== SWIPER POPULAR ===============*/
+var swiper = new Swiper(".popular__container", {
+
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        },
+});
 
 /*=============== VALUE ACCORDION ===============*/
 
