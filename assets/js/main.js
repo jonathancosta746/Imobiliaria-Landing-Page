45ef03cb7a8879ebf955c4b7b4df710c5cfb442b
Jonathan Costa <jonathancosta746@gmail.com>
2022-07-12 14:22:31 -0300

    adicionado javascript para exibir barra de navegação ao usar scroll

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/
+
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
