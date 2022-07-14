32c18d5ca11e07b6976ad1af2ed3ef6a8338ebf1
Jonathan Costa <jonathancosta746@gmail.com>
2022-07-14 10:58:01 -0300

    campo valores finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,8 +24,35 @@ var swiper = new Swiper(".popular__container", {
 });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
 
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value__accordion-header')
 
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if(openItem && openItem !== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item,classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+    }
+
+    
+}
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
